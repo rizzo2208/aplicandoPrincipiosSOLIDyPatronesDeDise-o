53889687cafda3cb4346d34ae0543e86cb8502ce
Nahuel Rizzo <52260647+rizzo2208@users.noreply.github.com>
2022-05-11 00:18:53 -0300

    Agregar archivos de proyecto.

---
+++ aplicandoPrincipiosSOLIDyPatronesDeDiseño/clase/Robot.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aplicandoPrincipiosSOLIDyPatronesDeDiseño.clase
+{
+    abstract class Robot
+    {
+        private string material;//METAL, PLASTICO O AMBAS
+        private string tipoCarga;// INTERNA O EXTERNA
+        private string sistemaDeControl; //AUTOMATICO O MANUAL 
+        private string codigoDeSerie;//ID, CLAVE PRIMARIA
+
+        //CONSTRUCTOR
+        public Robot(string material, string tipoCarga, string sistemaDeControl, string codigoDeSerie)
+        {
+            this.material = material;
+            this.tipoCarga = tipoCarga;
+            this.sistemaDeControl = sistemaDeControl;
+            this.codigoDeSerie = codigoDeSerie;
+        }
+
+
+        //METODOS
+        public abstract void getMaterial();
+        public abstract void getTipoCarga();
+        public abstract void getSistemaDeControl();
+        public abstract void getCodigoDeSerie();
+
+
+
+    }
+}
---
+++ aplicandoPrincipiosSOLIDyPatronesDeDiseño/clase/Terrestre.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using aplicandoPrincipiosSOLIDyPatronesDeDiseño.clase
+
+
+namespace aplicandoPrincipiosSOLIDyPatronesDeDiseño.clase
+{
+    internal class Terrestre : Robot
+    {
+        public Terrestre(string material, string tipoCarga, string sistemaDeControl, string codigoDeSerie) : base(string material, string tipoCarga, string sistemaDeControl, string codigoDeSerie)
+        {
+            
+        }
+
+
+
+
+
+        //METODOS HERENCIA
+
+        public override void getCodigoDeSerie()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void getMaterial()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void getSistemaDeControl()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void getTipoCarga()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
